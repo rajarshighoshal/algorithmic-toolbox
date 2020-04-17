879312780c4bc5af2815e50c55d11aee9766c612
rajarshighoshal <rajarshi.ghoshal2013@gmail.com>
2020-04-17 21:21:54 +0530

    created fractional knaosack in cpp

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
+#include <algorithm> 
+#include <functional> 
 #include <vector>
 
 using std::vector;
 
-double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
+double get_optimal_value(int n, int capacity, vector<int> weights, vector<int> values) {
   double value = 0.0;
-
-  // write your code here
-
+  vector<std::pair<double,int>> value_per_unit(n); 
+  for(int i = 0; i < n; i++) {
+    value_per_unit[i].first = double(values[i])/double(weights[i]);
+    value_per_unit[i].second = weights[i];
+  }
+  // sort from highest vale per unit to lowest value per unit
+  std::sort(value_per_unit.begin(), value_per_unit.end(), std::greater<std::pair<double,int>>());
+  for(int i = 0; i < n && capacity > 0; i++) {
+    value += std::min(value_per_unit[i].second, capacity) * value_per_unit[i].first;
+    capacity -= std::min(value_per_unit[i].second, capacity);
+  }
   return value;
 }
 
@@ -21,7 +31,7 @@ int main() {
     std::cin >> values[i] >> weights[i];
   }
 
-  double optimal_value = get_optimal_value(capacity, weights, values);
+  double optimal_value = get_optimal_value(n, capacity, weights, values);
 
   std::cout.precision(10);
   std::cout << optimal_value << std::endl;
