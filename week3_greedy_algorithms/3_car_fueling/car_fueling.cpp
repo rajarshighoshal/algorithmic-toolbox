5b71d2f3a7abd14e7e45d18e5189dc6c8cc4a1db
rajarshighoshal <rajarshi.ghoshal2013@gmail.com>
2020-04-18 21:34:18 +0530

    car fueling cpp file

--- week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
+++ week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 
 using std::cin;
@@ -6,9 +7,23 @@ using std::cout;
 using std::vector;
 using std::max;
 
+
 int compute_min_refills(int dist, int tank, vector<int> & stops) {
-    // write your code here
-    return -1;
+    stops.insert(stops.begin(), 0);
+    stops.push_back(dist);
+    int num_refills = 0;
+    int current_refill = 0;
+    while(current_refill <= stops.size()) {
+        int last_refill = current_refill;
+        while(current_refill <=  stops.size() && (stops[current_refill+1] - stops[last_refill]) <= tank) 
+            current_refill++;
+        if(current_refill == last_refill)
+            return -1;
+        if (current_refill <=  stops.size())
+            num_refills++;
+        
+    }
+    return num_refills;
 }
 
 
