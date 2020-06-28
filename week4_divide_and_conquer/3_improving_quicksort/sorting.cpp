99d3872af81cc1d3a09abf32aa500a6618642900
rajarshighoshal <rajarshi.ghoshal2013@gmail.com>
2020-06-29 01:20:04 +0530

    modified quicksort to quicksort3 in cpp

--- week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
+++ week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
@@ -18,6 +18,26 @@ int partition2(vector<int> &a, int l, int r) {
   return j;
 }
 
+vector<int> partition3(vector<int> &a, int l, int r) {
+  vector<int> m(2);
+  int x = a[l];
+  m[0] = l;
+  m[1] = r;
+  for (int i = l+1; i <= m[1];) {
+    if (a[i] < x) {
+      swap(a[i], a[m[0]]);
+      m[0]++;
+      i++;
+    }
+    else if (a[i] == x) i++;
+    else {
+      swap(a[i], a[m[1]]);
+      m[1]--;
+    }
+  }
+  return m;
+}
+
 void randomized_quick_sort(vector<int> &a, int l, int r) {
   if (l >= r) {
     return;
@@ -25,10 +45,10 @@ void randomized_quick_sort(vector<int> &a, int l, int r) {
 
   int k = l + rand() % (r - l + 1);
   swap(a[l], a[k]);
-  int m = partition2(a, l, r);
+  vector<int> m = partition3(a, l, r);
 
-  randomized_quick_sort(a, l, m - 1);
-  randomized_quick_sort(a, m + 1, r);
+  randomized_quick_sort(a, l, m[0] - 1);
+  randomized_quick_sort(a, m[1] + 1, r);
 }
 
 int main() {
