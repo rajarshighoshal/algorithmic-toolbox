72fdf7f49c8cc6ebdcfcab5d0a3e8199e987382d
rajarshighoshal <rajarshi.ghoshal2013@gmail.com>
2020-06-28 21:29:49 +0530

    majority elemnt implementation in c++

--- week4_divide_and_conquer/2_majority_element/majority_element.cpp
+++ week4_divide_and_conquer/2_majority_element/majority_element.cpp
@@ -7,7 +7,22 @@ using std::vector;
 int get_majority_element(vector<int> &a, int left, int right) {
   if (left == right) return -1;
   if (left + 1 == right) return a[left];
-  //write your code here
+
+  int mid = left + (right - left - 1) / 2;
+
+  int left_elm = get_majority_element(a, left, mid + 1);
+  int right_elm = get_majority_element(a, mid + 1, right);
+
+  int left_count = 0;
+  for(int i = left; i < right; i++) 
+    if(a[i] == left_elm) left_count++;
+  if(left_count > (right - left)/2) return left_elm;
+
+  int right_count = 0;
+  for(int i = left; i < right; i++) 
+    if(a[i] == right_elm) right_count++;
+  if(right_count > (right - left)/2) return right_elm;  
+  
   return -1;
 }
 
