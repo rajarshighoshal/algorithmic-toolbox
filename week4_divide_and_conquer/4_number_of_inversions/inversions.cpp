11a58214f200b62c6b0c25bec4898c983e8c625e
rajarshighoshal <rajarshi.ghoshal2013@gmail.com>
2020-07-03 02:04:38 +0530

    created number of inversions counter for a array in cpp

--- week4_divide_and_conquer/4_number_of_inversions/inversions.cpp
+++ week4_divide_and_conquer/4_number_of_inversions/inversions.cpp
@@ -3,13 +3,46 @@
 
 using std::vector;
 
+long long inversion_merge(vector<int> &a, vector<int> &b, size_t left, size_t mid, size_t right) {
+  long long count = 0;
+  size_t i = left, j = mid, k = left;
+  while(i < mid && j < right) {
+    if(a[i] <= a[j]) {
+      b[k] = a[i];
+      i++;
+      k++;
+    }
+    else {
+      b[k] = a[j];
+      count += mid -i;
+      j++;
+      k++;
+    }
+  }
+  while(i < mid) {
+    b[k] = a[i];
+    i++;
+    k++;
+  }
+  while(j < right) {
+    b[k] = a[j];
+    j++;
+    k++;
+  }
+
+  for (i=left; i < right; i++)
+			a[i] = b[i];
+
+  return count;
+}
+
 long long get_number_of_inversions(vector<int> &a, vector<int> &b, size_t left, size_t right) {
   long long number_of_inversions = 0;
   if (right <= left + 1) return number_of_inversions;
-  size_t ave = left + (right - left) / 2;
-  number_of_inversions += get_number_of_inversions(a, b, left, ave);
-  number_of_inversions += get_number_of_inversions(a, b, ave, right);
-  //write your code here
+  size_t mid = left + (right - left) / 2;
+  number_of_inversions += get_number_of_inversions(a, b, left, mid);
+  number_of_inversions += get_number_of_inversions(a, b, mid, right);
+  number_of_inversions += inversion_merge(a, b, left, mid, right);
   return number_of_inversions;
 }
 
