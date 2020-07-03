6a622b7bc115c3a8ae2393c74eea12171ec4934d
rajarshighoshal <rajarshi.ghoshal2013@gmail.com>
2020-07-03 20:04:05 +0530

    cpp solution for points_and_segments exercise

--- week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.cpp
+++ week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using std::vector;
 
-vector<int> fast_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
+vector<int> fast_count_segments(vector<std::pair<int,int>> &seg, vector<int> points) {
+  std::sort(seg.begin(), seg.end());
   vector<int> cnt(points.size());
-  //write your code here
-  return cnt;
-}
-
-vector<int> naive_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
-  vector<int> cnt(points.size());
-  for (size_t i = 0; i < points.size(); i++) {
-    for (size_t j = 0; j < starts.size(); j++) {
-      cnt[i] += starts[j] <= points[i] && points[i] <= ends[j];
+  size_t k;
+  for(size_t i = 0; i < points.size(); i++) {
+    k = 0;
+    for(size_t j = 0; j < seg.size(); j++) {
+      if(points[i] >= seg[j].first && points[i] <= seg[j].second)   k++;
+      else if(points[i] < seg[j].first)   j = seg.size();
     }
+    cnt[i] = k;
   }
   return cnt;
 }
 
 int main() {
-  int n, m;
+  size_t n, m, seg_start, seg_end;
   std::cin >> n >> m;
-  vector<int> starts(n), ends(n);
-  for (size_t i = 0; i < starts.size(); i++) {
-    std::cin >> starts[i] >> ends[i];
+  vector<std::pair<int,int>> seg(n);
+  for (size_t i = 0; i < seg.size(); i++) {
+    std::cin >> seg_start >> seg_end;
+    seg[i] = std::make_pair(seg_start, seg_end);
   }
   vector<int> points(m);
   for (size_t i = 0; i < points.size(); i++) {
     std::cin >> points[i];
   }
   //use fast_count_segments
-  vector<int> cnt = naive_count_segments(starts, ends, points);
+  vector<int> cnt = fast_count_segments(seg, points);
   for (size_t i = 0; i < cnt.size(); i++) {
     std::cout << cnt[i] << ' ';
   }
