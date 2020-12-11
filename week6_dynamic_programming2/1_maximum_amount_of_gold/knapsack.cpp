d17b34b34f386292bfee8a164161bdd77ad094a5
rajarshighoshal <rajarshi.ghoshal2013@gmail.com>
2020-12-11 20:31:14 +0530

    solved 0/1 knapsack problem using dynmic programming

--- week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
+++ week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <algorithm> 
 
 using std::vector;
 
-int optimal_weight(int W, const vector<int> &w) {
-  //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
+
+int optimal_weight(unsigned long W, const vector<unsigned long> &w) {
+  // create matrix for storing weights
+  vector<vector<unsigned long>> weights(w.size() + 1, vector<unsigned long> (W + 1, 0));
+
+  for(size_t i = 0; i <= w.size(); i++){
+    for(size_t j = 0; j <= W; j++){
+      if(i == 0 || j == 0)
+        weights[i][j] = 0;
+      else if(w[i-1] <= j)
+        weights[i][j] = std::max(w[i -1] + weights[i - 1][j - w[i - 1]], weights[i - 1][j]);
+      else
+        weights[i][j] = weights[i - 1][j];
     }
   }
-  return current_weight;
+  int max_weight = weights[w.size()][W];
+
+  return max_weight;
 }
 
 int main() {
-  int n, W;
+  unsigned long n, W;
   std::cin >> W >> n;
-  vector<int> w(n);
-  for (int i = 0; i < n; i++) {
+  vector<unsigned long> w(n);
+  for (size_t i = 0; i < n; i++) {
     std::cin >> w[i];
   }
   std::cout << optimal_weight(W, w) << '\n';
